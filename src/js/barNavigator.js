f57615ceaeca0d8a14f27802ae0aa8531befd085
humberto <humber.arraya@gmail.com>
2025-01-17 21:17:46 -0300

    refact: improve navigation toggle functionality and add shadow effect

--- src/js/barNavigator.js
+++ src/js/barNavigator.js
@@ -1,11 +1,14 @@
 const btnBar = document.querySelector(".btn-bar")
 const btnBarClose = document.querySelector(".btn-bar-close")
 const nav = document.querySelector("#navigator")
+const contentNavigator = document.querySelector("#navigator")
 
 btnBar.addEventListener('click', () => {
   nav.classList.toggle("hidden")
+  contentNavigator.classList.toggle("shadow")
 })
 
 btnBarClose.addEventListener('click', () => {
-  nav.classList.add("hidden")
+  nav.classList.toggle("hidden")
+  contentNavigator.classList.toggle("shadow")
 })
